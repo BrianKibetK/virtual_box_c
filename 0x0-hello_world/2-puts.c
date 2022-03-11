caf9ad45de16b6a0bdca3ff1fa5480af69b7aedb
Brian Kirui <brianrui723@gmail.com>
2022-03-12 00:45:00 +0300

    Syntax code for C

---
+++ 0x0-hello_world/2-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - phrase that prints using puts function
+ *
+ * return: 0 (success), non-zero (failed)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
