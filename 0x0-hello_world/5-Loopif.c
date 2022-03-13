aeceef2cb32848498fda65dfd96b3cb0e3fb2cdf
Brian Kirui <brianrui723@gmail.com>
2022-03-13 13:36:11 +0300

    Nested Else if Statement

--- 0x0-hello_world/5-Loopif.c
+++ 0x0-hello_world/5-Loopif.c
@@ -12,29 +12,29 @@ int main(void)
 	printf("Please enter you age: ");
 	/*The input is then stored in age*/
 	scanf("%d\n", &age);
-	if (age <= 12)
+	if (age > 0 && age <= 12)
 	{
 		printf("You are in the age group of a child\n");
 	}
-	else if (age <= 19)
+	else if (age > 12 && age <= 19)
 	{
 		printf("You are a teenager\n");
 	}
-	else if (age < 25)
+	else if (age > 19 && age <= 24)
 	{
 		printf("You are in the youth age group\n");
 	}
-	else if (age <= 64)
+	else if (age > 24 && age <= 64)
 	{
 		printf("You are in the adult age group\n");
 	}
-	else if (age <= 135)
+	else if (age > 64 && age <= 135)
 	{
 		printf("You are in the Seniors age group\n");
 	}
 	else if (age > 136)
 	{
-		printf("You've broken the guiness world record of the oldest person alive!\n");
+		printf("You've broken the world record of the oldest person alive!\n");
 	}
 	else
 	{
