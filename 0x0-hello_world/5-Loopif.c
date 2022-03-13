75d717bb87abaef114e1e274ec653a5ee81dca56
Brian Kirui <brianrui723@gmail.com>
2022-03-13 13:18:53 +0300

    Nested Else If

---
+++ 0x0-hello_world/5-Loopif.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+/**
+ * Main - If else, else if statements in C
+ *
+ * Return: Always 0 (Success), non-zero (Failed)
+ */
+int main(void)
+{
+	int age;
+
+	printf("Please enter you age: ");
+	/*The input is then stored in age*/
+	scanf("%d\n", &age);
+	if (age <= 12)
+	{
+		printf("You are in the age group of a child\n");
+	}
+	else if (age <= 19)
+	{
+		printf("You are a teenager\n");
+	}
+	else if (age < 25)
+	{
+		printf("You are in the youth age group\n");
+	}
+	else if (age <= 64)
+	{
+		printf("You are in the adult age group\n");
+	}
+	else if (age <= 135)
+	{
+		printf("You are in the Seniors age group\n");
+	}
+	else if (age > 136)
+	{
+		printf("You've broken the guiness world record of the oldest person alive!\n");
+	}
+	else
+	{
+		printf("Invalid entry please enter the right figure\n");
+	}
+	return (0);
+
+}
